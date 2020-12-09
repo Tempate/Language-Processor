bcf5504ea79357ea9ebb0bb436a863f25eefd5b9
Tempate <ddiazq28@gmail.com>
2020-12-09 13:14:50 +0100

    End of file symbol

--- core/syntactic.py
+++ core/syntactic.py
@@ -16,15 +16,18 @@ class SyntacticAutomata:
         self.grammar_table = pd.read_csv("api/context_free_grammar_table.csv")
         self.out_file = open(options["parse"], "w")
 
+        self.out_file.write("Descendente ")
+
+
     def run(self):
-        while self.stack and self.tokens:
+        while self.stack:
             if STATE_PATTERN.match(self.stack[0]):
-                rule = self.get_rule()
+                index, rule = self.get_rule()
 
                 if type(rule) != str:
-                    raise ValueError("Invalid symbol for rule " + self.stack[0])
+                    raise ValueError("Invalid symbol for rule " + str(index))
 
-                self.stack = self.parse_rule(rule) + self.stack[1:]
+                self.stack = rule.split() + self.stack[1:]
             else:
                 action = self.stack.pop(0)
 
@@ -36,20 +39,18 @@ class SyntacticAutomata:
                 if action != token.get_symbol():
                     raise ValueError("Token <%s, %d> doesn't match %s" % (token.type, token.attribute, action))
 
+
     def get_rule(self):
         row = self.grammar_table[(self.grammar_table["Nonterminal"] == self.stack[0])]
-        symbol = self.tokens[0].get_symbol()
+        symbol = self.tokens[0].get_symbol() if self.tokens else '$'
+        
+        index, rule = row[symbol].iloc[0].split(". ")
 
-        rule = row[symbol].iloc[0]
+        self.write_rule_index(index)
 
-        self.write_rule_index(row.index[0])
+        return index, rule
 
-        return rule
 
     def write_rule_index(self, index):
-        self.out_file.write("%d\n" % index)
-
-    @staticmethod
-    def parse_rule(rule):
-        return rule.split(" -> ")[1].split()
+        self.out_file.write("%d " % int(index))
      
